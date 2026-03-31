f1440df9e831d397059da97c274abc8749433808
Somotech <guido.rizzi@studiorizzi.net>
2026-03-31 15:58:55 +0200

    mar 31 mar 2026, 15:58:51, CEST

--- include/00-prototypes.h
+++ include/00-prototypes.h
@@ -10,7 +10,6 @@ extracting prototypes from  src/10-core-mbtp.c...
 // ****************************
 // src/10-core-mbtp.c
 // ****************************
-static bool modbus_receive_all(int sock, uint8_t *buffer, size_t length);
 int modbus_tcp_connect(const char *host, uint16_t port);
 int modbus_tcp_disconnect(int sock);
 uint16_t *modbus_read(int sock, int func, uint16_t start_address, uint16_t quantity);
@@ -18,7 +17,6 @@ uint16_t *modbus_read_coils(int sock, uint16_t start_address, uint16_t quantity)
 uint16_t *modbus_read_discrete_inputs(int sock, uint16_t start_address, uint16_t quantity);
 uint16_t *modbus_read_holding_registers(int sock, uint16_t start_address, uint16_t quantity);
 uint16_t *modbus_read_input_registers(int sock, uint16_t start_address, uint16_t quantity);
-static void modbus_client_task(void *pvParameters);
 void modbus_init(void);
 extracting prototypes from  src/10-core-mqtt.c...
 // ****************************
@@ -28,7 +26,6 @@ extracting prototypes from  src/10-core-wifi.c...
 // ****************************
 // src/10-core-wifi.c
 // ****************************
-static esp_err_t config_handler(httpd_req_t *req);
 void wifi_init();
 void start_web_server();
 extracting prototypes from  src/10-json-encoder.cc...
@@ -37,14 +34,11 @@ extracting prototypes from  src/10-json-encoder.cc...
 // ****************************
 void jsonClear();
 void jsonInit();
-static const char *jsonComma();
 const char *jsonGetBuffer();
 uint16_t jsonGetBufferSize();
 const char *jsonGetCompressedBuffer();
 const char *jsonGetEncryptedBuffer();
 uint16_t jsonGetCompressedSize();
-static void bpAddValue(const char vtype, const void *vvalue, const uint8_t vsize);
-static void jsonOpen(const char *oname, char bracket, char cbracket);
 void jsonClose();
 void jsonCloseAll();
 void jsonAddValue(int8_t value);
--- include/revision.h
+++ include/revision.h
@@ -1,1 +1,1 @@
-#define REV_DATE \"Tue Mar 31 15:57:47 2026 +0200\"" 
+#define REV_DATE \"Tue Mar 31 15:58:51 2026 +0200\"" 
