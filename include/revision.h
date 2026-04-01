af9e1c7b6581e650ae215a477a5cb7379de83f1d
Somotech <guido.rizzi@studiorizzi.net>
2026-04-01 21:31:07 +0200

    mer 1 apr 2026, 21:31:07, CEST

--- include/revision.h
+++ include/revision.h
@@ -1,1 +1,2 @@
-#define REV_DATE \"Wed Apr 1 19:23:47 2026 +0200\"" 
+#define REV_DATE "Wed Apr 1 21:27:54 2026 +0200" 
+#define REV_CURR "c761051"
--- src/20-rpcfunctions.c
+++ src/20-rpcfunctions.c
@@ -6,10 +6,10 @@
 void sysGetInfo(void) {
 
   //jsonAddObject("hw", String(BOARDID).c_str());
-  jsonAddObject_string("sn", );
-  //jsonAddObject("fw", REVISION);
+  jsonAddObject_string("sn",mac_str );
+  jsonAddObject_string("fw", REVISION);
   jsonAddObject_string("mac", mac_str);
-  //jsonAddObject("up", (uint32_t)(millis() / 1000));
+  jsonAddObject_uint32_t("up", (uint32_t)(millis() / 1000));
   jsonAddObject_string("ip", ipaddr);
   //String temp = String((temprature_sens_read() - 32) / 1.8);
   //jsonAddObject("te", temp.c_str());
